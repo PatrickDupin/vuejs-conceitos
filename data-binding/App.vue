bb406237cd8f57a63ff3095ceb4a4eaca4237655
patrick <sophia1493>
2021-02-19 09:23:32 -0300

    Conceitos Vue.js

---
+++ data-binding/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 v-bind:style="{ textDecoration: decoration }">Hello {{ name }}!</h1>
+        <input type="text" v-model="name" />
+        <br>
+        <a v-bind:href="link">Link para o curso!</a>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        name: 'Igor',
+        link: 'https://treinamento.vuejsbrasil.org',
+        decoration: 'underline'
+    })
+}
+</script>
+
+<style>
+</style>
