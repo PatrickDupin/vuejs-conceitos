8020982cfd01fdba4b4738a01fe278b125f620c1
patrick <sophia1493>
2021-02-19 10:14:34 -0300

    Conceitos Vue.js

---
+++ lifecycle-hooks/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Lifecycle hooks</h1>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+
+export default {
+    setup () {
+        onMounted ( () => {
+            console.log('Foi montado com composition API');
+        });
+    }
+}
+/*
+export default {
+    mounted () {
+        console.log('Foi montado com options API');
+    }
+}
+*/
+</script>
